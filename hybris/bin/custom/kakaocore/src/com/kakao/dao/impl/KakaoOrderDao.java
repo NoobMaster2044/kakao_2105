65372dc4963cb44178df9fd5c275cb392032c462
NoobMaster2044 <cristhoferoliverc@gmail.com>
2023-10-31 12:19:23 -0600

    Cambiendao estructura de carpetas

--- hybris/bin/custom/kakaocore/src/com/kakao/dao/impl/KakaoOrderDao.java
+++
@@ -1,9 +0,0 @@
-package com.kakao.dao.impl;
-
-import de.hybris.platform.core.model.order.OrderModel;
-
-import java.util.List;
-
-public interface KakaoOrderDao {
-    List<OrderModel> findOrdersPlacedThisHour();
-}
--- hybris/bin/custom/kakaocore/src/com/kakao/service/KakaoOrderService.java
+++
@@ -1,9 +0,0 @@
-package com.kakao.service;
-
-import de.hybris.platform.core.model.order.OrderModel;
-
-import java.util.List;
-
-public interface KakaoOrderService {
-    List<OrderModel> getOrdersPlacedThisHour();
-}
